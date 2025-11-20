6b9d8ffc8f435779c33294ddc841e114e0c066ce
原知輝 <tomontomon1203@icloud.com>
2025-11-20 14:03:15 +0900

    update

--- rust_coding/www/script.js
+++ rust_coding/www/script.js
@@ -10,7 +10,7 @@ window.onload = async function () {
 
 function showQuestion() {
     const quiz = quizData[currentIndex];
-    document.getElementById("code-block").innerHTML = quiz.question.replace(/\n/g,"<br>");
+    document.getElementById("code-block").innerHTML = quiz.question.replace(/\\n/g,"<br>")                                                                                     
     document.getElementById("result").textContent = "";
     document.getElementById("next-button").style.display = "none";
     updateStatus();
@@ -18,11 +18,10 @@ function showQuestion() {
 
 function checkAnswer(selected) {
     const quiz = quizData[currentIndex];
-    const result = document.getElementById("result");
-
+    const result = document.getElementById("result")
     if (selected === quiz.answer) {
         result.textContent = "正解！";
-        result.style.color = "lightgreen";
+        result.style.color = "lightblue";
         score++;
     } else {
         result.textContent = "残念、不正解！";
@@ -47,4 +46,17 @@ function loadNextQuestion() {
 function updateStatus() {
     document.getElementById("question-count").textContent = `${currentIndex + 1}問目`;
     document.getElementById("score").textContent = `正解数: ${score}`;
+}
+
+function setTheme(Theme) {
+    document.body.className = theme + '-theme';
+}
+
+function setTheme(theme) {
+    document.body.classList.remove("rust-theme", "js-theme");
+    if (theme === "rust") {
+        document.body.classList.add("rust-theme");
+    } else if (theme === "js") {
+        document.body.classList.add("js-theme");
+    }
 }
